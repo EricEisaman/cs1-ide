175257292846291a60f9689d85c9474e041ffb78
Glitch (cs1-ide) <none>
2019-07-16 14:33:33 +0000

    update lessons to have numbered names and added lesson 10

--- public/components/lessons/templates.mjs
+++ public/components/lessons/templates.mjs
@@ -1,66 +1,73 @@
 export const templates = {
 
-"Variables, Logging and Debugging": {
+"Lesson 1": {
   lessonURL: 'https://docs.google.com/presentation/d/e/2PACX-1vSaITNmVTyufVo-8aqI4nKK_pKuWA9j4StRdSYl66aoaI7Fmi4mPyWmsN5UEU9BP5jZwS8k2HFdoh1y/embed?start=false&loop=false&delayms=660000',
   quizURL: 'https://docs.google.com/forms/d/e/1FAIpQLSc7YF-ZQTdoB9akDJLdONEzn3pFGxxGFfPIeScVKqAKxDJ8DA/viewform?embedded=true',
   codeTemplate:false,
   saveTarget:'./src/core/components/my-components.js'
 },
 
-"Data Types in JavaScript": {
+"Lesson 2": {
   lessonURL: 'https://docs.google.com/presentation/d/e/2PACX-1vTQSm1t0LUtmmSljJU1SI2cjoDQuUOoRHW1Y6M5W2MuiePevJbdn4gSQzhnLDgJDA5IkxGBtjUQzY2u/embed?start=false&loop=false&delayms=333000',
   quizURL: 'https://docs.google.com/forms/d/e/1FAIpQLSfbDUh1E2XsvBYcNUxTHvAykWd8x-fn5g5Srjco9NmgcTyNmA/viewform?embedded=true',
   codeTemplate:false,
   saveTarget:'./src/core/components/my-components.js'
 },
 
-"JSON": {
+"Lesson 3": {
   lessonURL: 'https://docs.google.com/presentation/d/e/2PACX-1vSJ7cu1Qvzc8RuZ4oFVU1ys-Jm1xIC7J70GUXDosSt6JLq0yrjeL32tj_hJH1FO0J4fo7UhxEKP0H52/embed?start=false&loop=false&delayms=660000',
   quizURL: 'https://docs.google.com/forms/d/e/1FAIpQLSdqgU3M_oAbwIKEQiEkCMYW7lQjmQ1n5BiZzq3xdL2V7liQoA/viewform?embedded=true',
   codeTemplate:false,
   saveTarget:'./src/core/config/client-config.json'
 },
 
-"Creating Components Part 1": {
+"Lesson 4": {
   lessonURL: 'https://docs.google.com/presentation/d/e/2PACX-1vQiQQbRk3Q4wQ-1qXfkYRSpiR9fE5YFnd67UwaSN5X41y3Ok8VeX6zbj0frm9PvwPlSMLvDUfcnCNzh/embed?start=false&loop=false&delayms=333000',
   quizURL: 'https://docs.google.com/forms/d/e/1FAIpQLSeGqTf886LPvpElopcRg5G3CQBVQsoeMLzoVFOsTrovVtXINQ/viewform?embedded=true',
   codeTemplate:false,
   saveTarget:'./src/core/components/my-components.js'
 },
 
-"Using Components in HTML": {
+"Lesson 5": {
   lessonURL: 'https://docs.google.com/presentation/d/e/2PACX-1vRK75jfd-ol8p_h0YoWGHGNNTV7R_76TuXARf1N4nTgUR7F_b2cBg3DPSSheX2K-0d3vmLRH4ZmoIda/embed?start=false&loop=false&delayms=60000',
   quizURL: 'https://docs.google.com/forms/d/e/1FAIpQLSdsbgeYNCXfeLTQQ9e3Ebj7rhA-bZ-VRfjji7miJ0k-VYhYIQ/viewform?embedded=true',
   codeTemplate:false,
   saveTarget:'./public/index.html'
 },
 
-"Using Components in JS": {
+"Lesson 6": {
   lessonURL: 'https://docs.google.com/presentation/d/e/2PACX-1vQ4Q1zGdnh2xV4LgpztMo7EeMSM2jB0RHC8tRDjjVn9BfYt5m1LsIUd0ebLT0hX8BwVm7AURsl22wrv/embed?start=false&loop=false&delayms=333000',
   quizURL: 'https://docs.google.com/forms/d/e/1FAIpQLSe4FWqBU7t5xv84gKk_vTNwZRiKCTKwoqgfB9fp5SmIqF2-iw/viewform?embedded=true',
   codeTemplate:false,
   saveTarget:'./src/core/components/my-components.js'
 },
 
-"Functions in JavaScript": {
+"Lesson 7": {
   lessonURL: 'https://docs.google.com/presentation/d/e/2PACX-1vTqHCqPTWl-K3I0X70vN3X1jgyOSJWGuIQmkc4XvWusD9GgETAjS9eTRGhgfcrwk8Mnpq6qgjN4pDKT/embed?start=false&loop=false&delayms=60000',
   quizURL: 'https://docs.google.com/forms/d/e/1FAIpQLSf8_u5CzBPxXCbfVC28JBG9WVMVyP1xu3_tZNjiyg5_bP_7nw/viewform?embedded=true',
   codeTemplate:false,
   saveTarget:'./src/core/components/my-components.js'
 },
 
-"Boolean Expressions in JS": {
+"Lesson 8": {
   lessonURL: 'https://docs.google.com/presentation/d/e/2PACX-1vTWHy-erFeW4zNWJPZT_J1ZHmOrkROaCuwjisS7sgtArJsbP9nmnohVRgW8Wt-5fN0XcQzHyiMp9MWQ/embed?start=false&loop=false&delayms=60000',
   quizURL: 'https://docs.google.com/forms/d/e/1FAIpQLSd3YgDFptJ1LicRJ-dB3CI2ISfgnf8p095iKOAopBTJLw6mlA/viewform?embedded=true',
   codeTemplate:false,
   saveTarget:'./src/core/components/my-components.js'
 },
 
-"Creating Components Part 2": {
+"Lesson 9": {
   lessonURL: 'https://docs.google.com/presentation/d/e/2PACX-1vSQKHVBZ5qfq-mGG85RmI8NbU5U_y9_cTNXZYnA0edFw84XmExa9EQKQXGZcPiPEzRMc8SMmqK92FpO/embed?start=false&loop=false&delayms=60000',
   quizURL: 'https://docs.google.com/forms/d/e/1FAIpQLSfDjIQRW4s2jjlH1tDLCwjxoHbxUorgAa2Et_Ju8xNxeMpnsA/viewform?embedded=true',
   codeTemplate:false,
   saveTarget:'./src/core/components/my-components.js'
+},
+  
+"Lesson 10": {
+  lessonURL: 'https://docs.google.com/presentation/d/e/2PACX-1vRzlBxL80-ysJX5gwZcrQBr4MHBZND8xQP8vhlqWC9NCIuw-pcFIz9jQKX31rK2wexQ3AZkgCZj0Uur/embed?start=false&loop=false&delayms=60000',
+  quizURL: 'https://docs.google.com/forms/d/e/1FAIpQLSfkP4WYTS1l4KwG3zh5965qLBcg-lzy_fs2tV6hOoJQAdESew/viewform?embedded=true',
+  codeTemplate:false,
+  saveTarget:'./public/index.html'
 },
 
 
